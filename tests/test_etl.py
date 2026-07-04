870ac2cf31de90559d9c76b05767dadb23c893a7
Rachana R Kulkarni <rachanakulks@gmail.com>
2026-07-04 11:06:41 +0530

    Fix GitHub Actions

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Add project root to Python path
+# Add project root directory to Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import pytest
